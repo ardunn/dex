b6a1d0ef360ad656d99db4d33c9db21a9d9a80a6
ardunn <ardunn@lbl.gov>
2020-03-06 01:57:32 -0800

    update cmd

--- dionysus/cmd.py
+++ dionysus/cmd.py
@@ -1,6 +1,8 @@
 
 # dion configure [root dir]
 # dion work
-# dion project [projectid, like 1] work/rename/stats/prioritize/rename
+# dion project [projectid, like 1] work/rename/stats/prioritize/rename/complete
+# dion project new
 # dion task [taskid, like 1.3] work/rename/edit/stats/prioritize/deprioritize/view/complete
+# dion task new
 # dion stats
